02e511f8f1b52852f6daa0f87c4ebf41483a1d8d
enghongtan <31095472+enghongtan@users.noreply.github.com>
2017-09-04 17:30:47 +0800

    Add files via upload

--- models/user.js
+++ models/user.js
@@ -4,10 +4,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    username: String,
-    firstname: String,
-    lastname: String,
-    password: String
+    userid: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    admin: Boolean,
+    profilePic: String,
+    department: String
 }, {collection: 'users'});
 
 module.exports = mongoose.model('User', UserSchema);
